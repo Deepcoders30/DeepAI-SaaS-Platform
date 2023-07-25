d6633dbd7145d2ebbb58b729a4380bf9c94567bb
Deepcoders30 <umair.khan.rocko@gmail.com>
2023-07-26 02:41:26 +0530

    Add dashboard page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,6 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex items-center justify-center h-[100vh]">{children}</div>;
+};
+
+export default AuthLayout;
+
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
+
+const MobileSidebar = () => {
+    const [ismounted, setIsMounted]=useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true);
+    }, []);
+
+    if(!ismounted){
+        return null;
+    }
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+          <Sidebar /> 
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
